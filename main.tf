7316205fc13dc4ef2dfe6eff235122d061c0b7e5
OladeleO <58373625+OladeleO@users.noreply.github.com>
2021-12-11 19:13:43 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,9 +11,5 @@ resource "google_container_cluster" "my_vpc_native_cluster" {
    name                 = var.gke_cluster_name
    location             = var.zone
    initial_node_count   = 3
-   auto_create_subnetworks = false
-   mtu                     = 1460
-   network              = "default"
-   subnetwork           = "default"
-  
+   
 }
